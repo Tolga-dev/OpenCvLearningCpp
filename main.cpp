b768d3f5158c251b83bbacff30fc401d85da0858
Tolga-dev <orhan.konat.12@gmail.com>
2023-11-14 20:16:45 +0300

    general math

--- main.cpp
+++ main.cpp
@@ -21,43 +21,142 @@
         return EXIT_FAILURE;              \
     } \
 
+#define w 400
 
 using namespace cv;
 using namespace std;
 
 
-namespace ex1::theory
+// pattern
+namespace ex::theory
 {
-    Mat DiscreteFourierTransform(Mat& img, Mat& img2)
+    void LineDrawHelper(Mat img, Point s, Point e)
     {
 
-
-        return img;
     }
 
     int Test(const char * picPath, const char * picPath2)
     {
-        Mat img = imread(picPath);
-        Mat img2 = imread(picPath2);
 
-        CHECK_IMAGE_EMPTY(img, picPath)
+        Mat atom_image = Mat::zeros(w,w, CV_8UC3);
 
-        DiscreteFourierTransform(img, img2);
+        LineDrawHelper( atom_image, Point( 0, w/2 ), Point( w, w/2));
+
+        namedWindow("Drawing_Atom", WINDOW_AUTOSIZE);
+        imshow("Drawing_Atom", atom_image);
+        waitKey(0);
+
 
         return 0;
     }
 }
 
 
+// general learning is, drawing
+// line, ellopse, rectange, circle, filled polygon
+namespace ex1::theory
+{
+    void LineDrawHelper(Mat img, Point s, Point e)
+    {
+        int thickness = 10;
+        int lineType = LINE_8;
+        line( img,
+              s,
+              e,
+              Scalar( 255, 255, 255 ),
+              thickness,
+              lineType );
+
+    }
+    void EllipseDrawHelper( Mat img, double angle )
+    {
+        int thickness = 2;
+        int lineType = 8;
+        ellipse( img,
+                 Point( w/2, w/2 ),
+                 Size( w/4, w/16 ),
+                 angle,
+                 0,
+                 360,
+                 Scalar( 255, 0, 0 ),
+                 thickness,
+                 lineType );
+    }
+    void FilledCircleDrawHelper( Mat img, Point center )
+    {
+        circle( img,
+                center,
+                w/32,
+                Scalar( 0, 0, 255 ),
+                FILLED,
+                LINE_8 );
+    }
+
+    int Test(const char * picPath, const char * picPath2)
+    {
+
+        Mat atom_image = Mat::zeros(w,w, CV_8UC3);
+
+        LineDrawHelper( atom_image, Point( 0, w/2 ), Point( w, w/2));
+        FilledCircleDrawHelper( atom_image, Point( w/2, w/2 ));
+        EllipseDrawHelper( atom_image, 30);
+
+
+        namedWindow("Drawing_Atom", WINDOW_AUTOSIZE);
+        imshow("Drawing_Atom", atom_image);
+        waitKey(0);
+
+
+        return 0;
+    }
+}
+
+// Random generator and text with opencv
+//
+namespace ex2::theory
+{
+    void LineDrawHelper(Mat img, Point s, Point e)
+    {
+
+
+    }
+
+    int Test(const char * picPath, const char * picPath2)
+    {
+
+
+        // generally i prefer to make it as commented
+//        Mat atom_image = Mat::zeros(w,w, CV_8UC3);
+//
+//        LineDrawHelper( atom_image, Point( 0, w/2 ), Point( w, w/2));
+//
+//        namedWindow("Drawing_Atom", WINDOW_AUTOSIZE);
+//        imshow("Drawing_Atom", atom_image);
+//        waitKey(0);
+
+
+        return 0;
+    }
+}
+
+// it is a probability distribution in which every value between an interval form a to b is equally likely to occur
+// P(x1 < X < x2) = (x2 - x1) / (b - a)
+
+// example
+// if a dolphins are uniformly distributes between 100 - 150
+// if we select a random dolphin, we can use a formula
+// to determine the probablity that the chosen dolphin wil weigh between 120 and 130 pounds
+
+float UniformDistribution(uint dmax, uint dmin, uint maxPossibleVal, uint minPossibleVal)
+{
+    return ((float)(dmax - dmin)/(float)(maxPossibleVal - minPossibleVal));
+}
+
 
 
 int main()
 {
-
-    const char * pic1Path = "/home/xamblot/CLionProjects/OpencvLearningCpp/asset/pic1.jpg";
-    const char * pic2Path = "/home/xamblot/CLionProjects/OpencvLearningCpp/asset/pic2.png";
-
-    ex1::theory::Test(pic1Path, pic2Path);
+    cout << UniformDistribution(130, 120, 150, 100) << endl;
 
     return 0;
 }
